832cda13c87d207c98c102353da7c2bc53ce61c6
Gabriel Saldanha <130391676+gbn0@users.noreply.github.com>
2024-08-31 10:37:16 -0300

    fix: fixed some returns and some endpoint docs

--- src/common/providers/services/event/fetch-events.service.ts
+++ src/common/providers/services/event/fetch-events.service.ts
@@ -9,29 +9,32 @@ export class FetchEventsService {
     constructor(private readonly database: PrismaService) {}
 
     async fetch_events(
-        user_id: string,
+        user_id: string | null,
     ): Promise<Result<FetchEventsModel[], string>> {
         try {
+            console.log(user_id);
             const events = await this.database.event.findMany();
-            const user = await this.database.user.findUnique({
-                where: { id: user_id },
-                include: {
-                    events: true
-                }
-            });
-
-            console.log('passa o findUnique');  
-            if (!user) {
-                return Err('User not found');
-            }
-
-            user.events.forEach(event => {
-                events.forEach(e => {
-                    if (e.event_id === event.event_id) {
-                        e.isRegistered = true;
+            if (user_id !== '{id}') {
+                const user = await this.database.user.findUnique({
+                    where: { id: user_id },
+                    include: {
+                        events: true
                     }
                 });
-            });
+
+                if (!user) {
+                    return Err('User not found');
+                }
+    
+                user.events.forEach(event => {
+                    events.forEach(e => {
+                        if (e.event_id === event.event_id) {
+                            e.isRegistered = true;
+                        }
+                    });
+                });
+            }
+            
 
             const formattedEvents = events.map(event => {
                 const formattedDate = DateTime.fromJSDate(event.date)
