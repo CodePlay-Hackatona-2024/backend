be05da4e2969d5523b7250bee29b9f2d0998d73a
Gabriel Saldanha <130391676+gbn0@users.noreply.github.com>
2024-08-31 10:23:03 -0300

    feat: Added endpoint for buying items

--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -6,11 +6,13 @@ import { LoginUserService } from "src/common/providers/services/user/login-user.
 import { LoginUserController } from "src/common/controllers/user/login-user.controller";
 import { RegisterInEventController } from "src/common/controllers/user/register-in-event.controller";
 import { RegisterInEventService } from "src/common/providers/services/user/register-in-event.service";
+import { UpdateBalanceService } from "src/common/providers/services/user/update-balance.service";
+import { UpdateBalanceController } from "src/common/controllers/user/update-balance.controller";
 
 @Module({
     imports: [PrismaModule],
-    providers: [CreateUserService, LoginUserService, RegisterInEventService],
-    controllers: [CreateUserController, LoginUserController, RegisterInEventController]
+    providers: [CreateUserService, LoginUserService, RegisterInEventService, UpdateBalanceService],
+    controllers: [CreateUserController, LoginUserController, RegisterInEventController, UpdateBalanceController]
 })
 
 export class UserModule {}
